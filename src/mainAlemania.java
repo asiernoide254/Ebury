07006051d79edcf65643b3580e2de7fe86624541
Chr1s2205 <63378128+Chr1s2205@users.noreply.github.com>
2021-12-08 20:24:07 +0100

    Update mainAlemania.java

--- src/mainAlemania.java
+++ src/mainAlemania.java
@@ -2,7 +2,7 @@ public class mainAlemania {
     public static void main(String[] args) {
         System.out.println("Agustín Valverde Ramos. <3");
         System.out.println("ola");
-        System.out.println("kaka");
+
 
     }
 }
