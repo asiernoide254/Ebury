74c811dbdc4088678710841397fd4b36e0a725d7
angelfdezcivila <angelfdezcivila@uma.es>
2021-12-11 19:41:19 +0100

    Inicio de implementacion de los botones de PrincipalAlemania.java con el main anterior en caso de querer cambiarlo

--- src/View/Alemania/PrincipalAlemania.java
+++ src/View/Alemania/PrincipalAlemania.java
@@ -2,14 +2,18 @@ package View.Alemania;
 
 import javax.swing.*;
 import javax.swing.plaf.nimbus.NimbusLookAndFeel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class PrincipalAlemania {
+public class PrincipalAlemania extends JFrame{
 
     private JPanel panel1;
     private JButton informeInicialButton;
     private JButton informeSemanalButton;
     private JLabel Imagen;
 
+    /**
+    //no hereda de JFrame
     public static void main(String[] args) {
         JFrame frame = new JFrame("Ebury");
         frame.setContentPane(new PrincipalAlemania().panel1);
@@ -19,4 +23,47 @@ public class PrincipalAlemania {
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
+    */
+
+    public PrincipalAlemania(String title){
+        super(title);
+        setContentPane(panel1);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setIconImage(new ImageIcon("Recursos/Ebury.png").getImage());
+
+        informeInicialButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onInicial();
+            }
+        });
+
+        informeSemanalButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onSemanal();
+            }
+        });
+
+    }
+
+    private void onInicial() {
+        // add your code here
+        Error dialog = new Error();
+        dialog.pack();
+        dialog.setLocationRelativeTo(null);
+        dialog.setVisible(true);
+    }
+
+    private void onSemanal() {
+        // add your code here if necessary
+        dispose();
+    }
+
+    public static void main(String[] args) {
+        PrincipalAlemania frame = new PrincipalAlemania("Ebury");
+
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
 }
