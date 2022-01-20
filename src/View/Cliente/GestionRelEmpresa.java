c0c8db7cfcc7ebec6dfb04845f938d06ebae8c25
asiernoide254 <0619930829@uma.es>
2022-01-20 20:54:08 +0100

    GUI mejorada.

---
+++ src/View/Cliente/GestionRelEmpresa.java
@@ -0,0 +1,33 @@
+package View.Cliente;
+
+import com.toedter.calendar.JDateChooser;
+
+import javax.swing.*;
+
+public class GestionRelEmpresa {
+    private JPanel panel1;
+    private JLabel DatosP;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JPanel panelCal;
+    private JDateChooser JDateChooser1;
+    private JTextField textField6;
+    private JTextField textField7;
+    private JTextField textField8;
+    private JTextField textField9;
+    private JTextField textField10;
+    private JTextField textField11;
+    private JTextField textField12;
+    private JCheckBox válidaDirecciónActualCheckBox;
+    private JPasswordField passwordField1;
+    private JPasswordField passwordField2;
+    private JButton añadirButton;
+    private JButton cancelarButton;
+    private JComboBox comboBox1;
+    private JTable table1;
+    private JButton borrarButton;
+    private JButton finalizarButton;
+}
