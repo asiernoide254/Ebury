c91e1d762c8989cdaa74205cb17437405ef312dd
asiernoide254 <0619930829@uma.es>
2022-01-20 20:32:48 +0100

    GUI registro empresa/persona completado.

--- src/View/Cliente/RegistroEmpresa.java
+++ src/View/Cliente/RegistroEmpresa.java
@@ -1,7 +1,26 @@
 package View.Cliente;
 
+import com.toedter.calendar.JDateChooser;
+
 import javax.swing.*;
 
 public class RegistroEmpresa {
     private JPanel panel1;
+    private JLabel DatosP;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField6;
+    private JTextField textField7;
+    private JTextField textField8;
+    private JTextField textField9;
+    private JTextField textField10;
+    private JTextField textField11;
+    private JTextField textField12;
+    private JCheckBox válidaDirecciónActualCheckBox;
+    private JPasswordField passwordField1;
+    private JPasswordField passwordField2;
+    private JButton registrarseButton;
+    private JButton cancelarButton;
+    private JPanel panelBotones;
+    private JPanel panelInterior;
 }
